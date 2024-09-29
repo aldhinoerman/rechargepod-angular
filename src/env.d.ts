db6cf8dbc577601d33b84d2529b9e989f52d6553
aldhinoerman <aldynoerman@gmail.com>
2024-09-29 19:45:11 +0800

    angular env fixing

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,10 @@ declare interface Env {
   readonly NODE_ENV: string;
   readonly NG_APP_PUBLIC_NYT_API_KEY: string;
 }
+
+interface ImportMeta {
+  env: {
+    NG_APP_PUBLIC_NYT_API_KEY: string;
+    [key: string]: string;
+  };
+}
